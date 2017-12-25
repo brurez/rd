c0c654adf525fe2cd4047421dadfd6c51154fc04
Bruno de Rezende <brunoderezende@hotmail.com>
2017-12-25 13:14:30 -0200

    api to add contact without refresh page

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Contact = mongoose.model('contacts');
 const Visit = mongoose.model('visits');
-const { apiPost, apiGet } = require('./_helpers');
+const { apiGet } = require('./_helpers');
 
 const routes = app => {
 
@@ -32,7 +32,7 @@ const routes = app => {
 
   // INSERE UM
   app.post('/api/contacts', (req, res) => {
-    apiPost(req, Contact)
+    contactInsert(req.body)
       .then(contact => {
         res.send(contact);
       })
@@ -40,5 +40,38 @@ const routes = app => {
   });
 
 };
+
+function contactInsert(body){
+  return new Promise((resolve, reject) => {
+    const { uuid, name, email } = body;
+
+    if (email) {
+      // Usuário com cadastro ou com requisitos para cadastrar-se
+      // Insira nova visita relacionanda com contato
+      Contact.findOne({ email }, (err, contact) => {
+        if (!contact) {
+          // usuario não casdastrado nos contatos, adicionando contato e depois visita
+          const newContact = new Contact({
+            name,
+            email,
+          });
+          newContact.save((err, contact) => {
+
+            // Procura todas as visitas anonimas com o mesmo uuid e adiciona contato
+
+            Visit.updateMany(
+              {uuid, _contact: {$exists: false}},
+              {$set: {_contact: contact.id}},
+              (err, res) => {
+                if(err) reject(err);
+                resolve(res);
+              },
+            );
+          });
+        }
+      });
+    }
+  });
+}
 
 module.exports = { routes };
